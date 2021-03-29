b00fa2053fcc6ba88371bd3c54457b598b222014
helman101 <helmanandresortegon@gmail.com>
2021-03-29 15:42:21 +0200

    add Foods class and four foods with images

--- src/menu.js
+++ src/menu.js
@@ -1,10 +1,33 @@
+class Foods {
+  constructor(name, price, image)  {
+    this.name = name;
+    this.price = price;
+    this.image = image;
+  }
+}
+
+const bandejaPaisa = new Foods('Bandeja Paisa', '5', '../src/img/bandeja-paisa.jpeg');
+const ajiaco = new Foods('Ajiaco', '3', '../src/img/ajiaco.jpg');
+const tamal= new Foods('Tamal', '2', '../src/img/tamal.jpg');
+const arepa = new Foods('Arepa', '1.50', '../src/img/arepa.jpg');
+
+const menuArray = [bandejaPaisa, ajiaco, tamal, arepa]
+
 let showMenu = () => {
   let displayDiv = document.querySelector('#display');
-  let displayPara = document.createElement('p');
-
-  displayPara.textContent = 'Menu';
   displayDiv.innerHTML = '';
-  displayDiv.appendChild(displayPara);
+  displayDiv.classList.add('flex-wrap')
+  for(let i = 0; i < menuArray.length; i+=1){
+    const div = document.createElement('div');
+    let name = document.createElement('div');
+    div.classList.add('food');
+    name.textContent = menuArray[i].name;
+    div.appendChild(name);
+    div.style.backgroundImage = `url(${menuArray[i].image})`;
+    
+
+    displayDiv.appendChild(div);
+  }
 }
 
 export default showMenu
